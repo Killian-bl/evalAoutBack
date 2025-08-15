c5ffbe48f46f6e1ba2e4935f7837d20bd71ca29b
Killian-bl <kilian.bastardy@gmail.com>
2025-08-15 12:31:39 +0200

    premier commit creation des fichiers

---
+++ evalAout/src/main/java/com/greta/evalAout/EvalAoutApplication.java
@@ -0,0 +1,13 @@
+package com.greta.evalAout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalAoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvalAoutApplication.class, args);
+	}
+
+}
---
+++ evalAout/src/test/java/com/greta/evalAout/EvalAoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greta.evalAout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalAoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
