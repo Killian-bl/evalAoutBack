22e40b66a5d4623938a367801a7b845688d6c1da
Killian-bl <kilian.bastardy@gmail.com>
2025-08-15 17:47:38 +0200

    ajout des 2 dernier controlleur + cors + application

--- evalAout/src/main/java/com/greta/evalAout/Controllers/CustomerController.java
+++ evalAout/src/main/java/com/greta/evalAout/Controllers/CustomerController.java
@@ -1,5 +1,58 @@
 package com.greta.evalAout.Controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greta.evalAout.Dtos.CustomerDTO;
+import com.greta.evalAout.Entity.Customer;
+import com.greta.evalAout.Service.CustomerService;
+
+import jakarta.validation.Valid;
+
+
+
+
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
-    
+     private final CustomerService customerService;
+
+     public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+     }
+
+     @GetMapping("/customer/search")
+     public ResponseEntity<List<Customer>> searchCustomer(@RequestParam String email) {
+        List<Customer> customer = customerService.searchCustomer(email);
+         return ResponseEntity.ok(customer);
+     }
+
+     @DeleteMapping("/{customerId}/customer")
+     public ResponseEntity<Void> deleteCustomer(@PathVariable Long customerId) {
+        customerService.deleteCustomer(customerId);
+        return ResponseEntity.noContent().build();
+     }
+
+     @PutMapping("customer/{customerId}")
+     public ResponseEntity<CustomerDTO> updateCustomer(@PathVariable Long customerId, @Valid @RequestBody CustomerDTO dto) {
+         Customer updated = customerService.createCustomer(dto);
+         return ResponseEntity.ok(new CustomerDTO(updated));
+     }
+
+     @PostMapping("/customer")
+     public ResponseEntity<CustomerDTO> createCustomer(@Valid @RequestBody CustomerDTO dto) {
+         Customer customer = customerService.createCustomer(dto);
+         return ResponseEntity.ok(new CustomerDTO(customer));
+     }
+
 }
--- evalAout/src/main/java/com/greta/evalAout/Controllers/OrderController.java
+++ evalAout/src/main/java/com/greta/evalAout/Controllers/OrderController.java
@@ -1,5 +1,60 @@
 package com.greta.evalAout.Controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greta.evalAout.Dtos.OrderDTO;
+import com.greta.evalAout.Entity.Order;
+import com.greta.evalAout.Service.OrderService;
+
+import jakarta.validation.Valid;
+
+
+
+
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+    
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/order/search")
+    public ResponseEntity<List<Order>> searchOrder(@RequestParam Long orderId) {
+        List<Order> order = orderService.searchOrder(orderId);
+        return ResponseEntity.ok(order);
+    }
+
+    @DeleteMapping("/{orderId}/order")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
+        orderService.deleteOrder(orderId);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("order/{orderId}")
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Long orderId, @Valid @RequestBody OrderDTO dto) {
+        Order updated = orderService.updateOrder(orderId, dto);
+        return ResponseEntity.ok(new OrderDTO(updated));
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<OrderDTO> createOrder(@Valid @RequestBody OrderDTO dto) {
+        Order order = orderService.createOrder(dto);
+        return ResponseEntity.ok(new OrderDTO(order));
+    }
+    
     
 }
