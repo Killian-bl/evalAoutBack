056fbdc9dca4d3bcfaec5cb13e4ccd2312aa39f7
Killian-bl <kilian.bastardy@gmail.com>
2025-08-15 17:01:52 +0200

    orderService et productController

--- evalAout/src/main/java/com/greta/evalAout/Controllers/ProductController.java
+++ evalAout/src/main/java/com/greta/evalAout/Controllers/ProductController.java
@@ -1,5 +1,59 @@
 package com.greta.evalAout.Controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greta.evalAout.Dtos.ProductDTO;
+import com.greta.evalAout.Entity.Product;
+import com.greta.evalAout.Service.ProductService;
+
+import jakarta.validation.Valid;
+
+
+
+
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
+    
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    
+    @GetMapping("/product/search")
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam String name) {
+        List<Product> product = productService.searchProduct(name);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{productId}/product")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @PutMapping("product/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long productId, @Valid @RequestBody ProductDTO dto) {
+        Product updated = productService.updateProduct(productId, dto);
+        return ResponseEntity.ok(new ProductDTO(updated));
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO dto) {
+        Product product = productService.createProduct(dto);
+        return ResponseEntity.ok(new ProductDTO(product));
+    }
     
 }
--- evalAout/src/main/java/com/greta/evalAout/Service/OrderService.java
+++ evalAout/src/main/java/com/greta/evalAout/Service/OrderService.java
@@ -1,5 +1,54 @@
 package com.greta.evalAout.Service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.greta.evalAout.Dtos.OrderDTO;
+import com.greta.evalAout.Entity.Order;
+import com.greta.evalAout.Repositories.OrderRepositories;
+
+@Service
 public class OrderService {
     
+    private final OrderRepositories orderRepositories;
+
+    public OrderService(OrderRepositories orderRepositories) {
+        this.orderRepositories = orderRepositories;
+    }
+
+    public Order getOrderById(Long orderId) {
+        return orderRepositories.findByOrderId(orderId)
+        .stream().findFirst().orElseThrow(() -> new RuntimeException("Commande non trouvé"));
+    }
+
+    public List<Order> getAllOrder() {
+        return orderRepositories.findAll();
+    }
+
+    public List<Order> searchOrder(Long orderId) {
+        return orderRepositories.findByOrderId(orderId);
+    }
+
+    public List<Order> deleteOrder(Long orderId) {
+        orderRepositories.deleteById(orderId);
+        return orderRepositories.findAll();
+    }
+
+    public Order updateOrder(Long orderId, OrderDTO dto) {
+        Order order = orderRepositories.findById(orderId)
+        .orElseThrow(() -> new RuntimeException("Commande non trouvé"));
+        order.setDate(dto.getDate());
+        order.setCartPrice(dto.getCartPrice());
+        order.setProductNumber(dto.getProductNumber());
+        return orderRepositories.save(order);
+    }
+
+    public Order createOrder(OrderDTO dto) {
+        Order order = new Order();
+        order.setDate(dto.getDate());
+        order.setCartPrice(dto.getCartPrice());
+        order.setProductNumber(dto.getProductNumber());
+        return orderRepositories.save(order);
+    }
 }
